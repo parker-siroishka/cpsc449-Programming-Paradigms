7f48a4ade640cd45c7a62a4cc6502561198660ca
ryan.pang <ryan.pang@zone44-wc.cs.ucalgary.ca>
2019-01-21 13:50:07 -0700

    Added Class and Main
    Signed-off-by: ryan.pang <ryan.pang@zone44-wc.cs.ucalgary.ca>

---
+++ Assignment-1/Assignment-1/src/MachineTaskCalc/MachineTaskCalculator.java
@@ -0,0 +1,5 @@
+package MachineTaskCalc;
+
+public class MachineTaskCalculator {
+
+}
---
+++ Assignment-1/Assignment-1/src/MachineTaskCalc/main.java
@@ -0,0 +1,10 @@
+package MachineTaskCalc;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
