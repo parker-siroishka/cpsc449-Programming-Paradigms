65bee85b5795a05aad46dde22d2772d439ecc911
Rpyaanng <RyanPang12@gmail.com>
2019-03-19 22:40:41 +0500

    Update parse.hs

--- Assignment-2/parse.hs
+++ Assignment-2/parse.hs
@@ -3,76 +3,103 @@ import System.Environment
 import System.Exit
 import Data.Char
 import Data.List
-import Data.Typeable
-import Data.Maybe
+import Data.Tuple
 
 
--- Chop input.txt file into sublists of data between headers
-slice :: Int -> Int -> [String] -> [String]
+forced = [(1,'A')]
+forbidden = [(2, 'B')]
+tooNear = [('H', 'C')]
+machPens = [[1,10,10,10,10,10,10,10],[10,1,10,10,10,10,10,10],[10,10,1,10,10,10,10,10],[10,10,10,1,10,10,10,10],[10,10,10,10,1,10,10,10],[10,10,10,10,10,1,10,10],[10,10,10,10,10,10,1,10],[10,10,10,10,10,10,10,1]]
+tooNearPens = [('F', 'G', 100)]
 
-slice from to xs = take (to - from + 1) (drop from xs)
- 
 
-
-
--- Main code execution, where all header checks happen
 main = do
-    args <- getArgs
-    -- If CLI arguments are in wrong format exit
-    if  length (args) /= 2 then do
-        putStrLn("usage: <input file> <output file>")
-        exitFailure
-    else do
-        content <- readFile (head args)
-        -- Put input.txt (arg[0]) into [String]
-        let text = lines content
-
-        -- Get indices of all headers by converting to Int from Maybe Int. 
-        -- If header specified as below is not in the input.txt file, return -1 as fromMaybe
-        -- function cannot convert the index if it does not exist
-        let nameInd = fromMaybe (-1) (elemIndex "Name:" text)
-        let fpaInd = fromMaybe (-1) (elemIndex "forced partial assignment:" text)
-        let fmInd = fromMaybe (-1) (elemIndex "forbidden machine:" text)
-        let tntInd = fromMaybe (-1) (elemIndex "too-near tasks:" text)
-        let mpInd = fromMaybe (-1) (elemIndex "machine penalties:" text)
-        let tnpInd = fromMaybe (-1) (elemIndex "too-near penalities" text)
+    let all = permutations "ABCDEFGH"
+    let forcedAcc = getFAccepted forced forbidden all []
+    let allAccepted = getTooNearAccepted tooNear forcedAcc []
+    --print tooNearAccepted
+    --findBestMatch 
+    let bestMatch = findBestMatch allAccepted tooNearPens machPens 2147483647 []
+    print (printOutput bestMatch)
+    -- let machinePenalties = getMachinePenalties machPens "ABCDEFGH" 0
+    -- let tooNearPenalties = getTooNearPenalties tooNearPens "ABCDEFGH" machinePenalties
+    -- print tooNearPenalties
 
 
-        -- Measure length of input.txt String list. This is to be used to find last index of list.
-        let endOflist = length text
+-- machine = “ABCDEFGH”
 
-    	-- Add all header indices to their own list
-        let headerIndices = nameInd:fpaInd:fmInd:tntInd:mpInd:tnpInd:[]
-        -- If -1 is not present in headerIndices list, headers are of valid format
-        let valid  = not (-1 `elem` headerIndices)
+-- findIndex1 :: [(Integer, Char)] -> [Char] -> Integer -> Integer
+-- findIndex1 tuple [] index = -1
+-- findIndex1 tuple (x:xs) index 
+--     | get2nd(tuple) == x = index
+--     | otherwise = findIndex1 tuple xs (index + 1)
 
-        -- If headers valid, splice input.txt on each subsection of data inbetween header indices.
-        if valid then do
-            let nameList = slice (nameInd+1) (fpaInd-1) text
-            let fpaList = slice (fpaInd+1) (fmInd-1) text
-            let fmList = slice (fmInd+1) (tntInd-1) text
-            let tntList = slice (tntInd+1) (mpInd-1) text
-            let mpList = slice (mpInd+1) (tnpInd-1) text
-            let tnpList = slice (tnpInd+1) ((endOflist-1)) text
-            -- Print all sublists
-            putStrLn $ show (nameList)
-            putStrLn $ show (fpaList)
-            putStrLn $ show (fmList)
-            putStrLn $ show (tntList)
-            putStrLn $ show (mpList)
-            putStrLn $ show (tnpList)
+printOutput x = "Solution " ++ (fst x) ++ "; Quality: " ++ (show (snd x)) ++ "\n"
 
-        -- Else file format is invalid, and we write "Error while parsing to output.txt"
-        else do
-            file_out <- writeToFile (last args) valid
-            exitFailure
-        return 0
 
--- Write to file routine that gets executed if headers are in wrong format.
-writeToFile file_name valid = do
-    fileToWrite <- openFile file_name WriteMode
-    if valid then
-        hPutStrLn fileToWrite("")
-    else 
-    	hPutStrLn fileToWrite ("Error while parsing")
-    hClose fileToWrite
+
+stringIsAccepted :: [(Int, Char)] -> [Char] -> Bool
+stringIsAccepted [] machine = True
+stringIsAccepted (x:xs) machine = (((elemIndices(snd(x)) machine) == [fst(x) -1]) && stringIsAccepted xs machine)
+
+stringIsNotForbidden  :: [(Int, Char)] -> [Char]-> Bool
+stringIsNotForbidden [] machine = True
+stringIsNotForbidden (x:xs) machine = (((elemIndices(snd(x)) machine) /= [fst(x) -1]) && stringIsNotForbidden xs machine)
+
+
+-- Takes in the forced penalties, the set of all possible strings, the set of all allowed strings so far, and returns a set of all allowed strings
+getFAccepted :: [(Int, Char)] -> [(Int, Char)] -> [[Char]] -> [[Char]] -> [[Char]]
+
+getFAccepted (x:xs) (w:ws) [] accepted = accepted
+getFAccepted (x:xs) (w:ws) (y:ys) accepted
+    | (stringIsAccepted (x:xs) y) && stringIsNotForbidden (w:ws) y = getFAccepted (x:xs) (w:ws) ys (accepted ++ [y])
+    | otherwise = getFAccepted (x:xs) (w:ws) ys accepted
+
+
+-- Takes in too near tasks and machine string, returns bool.
+stringTooNear :: [(Char, Char)] -> [Char] -> Bool
+stringTooNear [] machine = True
+--Checks if indices are 7 and 0, checks if indices are x and x-1, checks rest of too near
+stringTooNear (x:xs) machine = (stringTooNear xs machine) && not (elemIndices(fst(x)) machine == [7] && elemIndices(snd(x)) machine == [0]) && not (elemIndices(fst(x)) machine!!0 == (elemIndices(snd(x)) machine!!0)-1)
+
+
+-- Takes in the too near tasks, the set of all possible forced strings, the set of all allowed too near tasks and returns the set of all allowed strings recursively
+getTooNearAccepted :: [(Char, Char)] -> [[Char]] -> [[Char]] -> [[Char]]
+
+getTooNearAccepted (x:xs) [] accepted = accepted
+getTooNearAccepted (x:xs) (y:ys) accepted
+    | stringTooNear (x:xs) y = getTooNearAccepted (x:xs) ys accepted ++ [y]
+    | otherwise = getTooNearAccepted (x:xs) ys accepted
+
+
+--takes in a 2D array of integers, the machine string, and the total penalty so far and returns the total machine penalty
+getMachinePenalties :: [[Int]] -> [Char] -> Int -> Int
+stringRef = "ABCDEFGH"
+getMachinePenalties matrix [] pen = 0
+getMachinePenalties matrix (y:ys) pen = (getMachinePenalties matrix ys pen) + (matrix!!(8 - length (y:ys)))!!((elemIndices y stringRef)!!0)
+
+
+extractFirst :: (a, b, c) -> a
+extractFirst (a,_,_) = a
+
+extractSecond :: (a, b, c) -> b
+extractSecond (_,b,_) = b
+
+extractThird :: (a, b, c) -> c
+extractThird (_,_,c) = c
+
+
+getTooNearPenalties :: [(Char, Char, Int)] -> [Char] -> Int -> Int
+getTooNearPenalties [] machine pen = pen
+getTooNearPenalties (x:xs) machine pen
+    | (elemIndices(extractFirst(x)) machine == [7] && elemIndices(extractSecond(x)) machine == [0]) = (getTooNearPenalties xs machine pen) + extractThird(x)
+    | (elemIndices(extractFirst(x)) machine!!0 == (elemIndices(extractSecond(x)) machine!!0)-1) = (getTooNearPenalties xs machine pen) + extractThird(x)
+    | otherwise = getTooNearPenalties xs machine pen
+
+
+findBestMatch :: [[Char]] -> [(Char, Char, Int)] -> [[Int]] -> Int -> [Char] ->([Char],Int)
+findBestMatch [] tooNearPenalties matrix tot best = (best,tot)
+findBestMatch (x:xs) tooNearPenalties matrix tot best
+    | getTooNearPenalties tooNearPenalties x (getMachinePenalties matrix x 0) <= tot = findBestMatch xs tooNearPenalties matrix (getTooNearPenalties tooNearPenalties x (getMachinePenalties matrix x 0)) x
+    | otherwise = findBestMatch xs tooNearPenalties matrix tot best
+	
